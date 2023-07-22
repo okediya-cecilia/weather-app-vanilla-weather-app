ae3e760d139c7685c4f80f717293abc80ac7a171
okediya-cecilia <139346561+okediya-cecilia@users.noreply.github.com>
2023-07-22 23:41:39 +0100

    updated the entire html page

--- Vanilla-weather-app/src/app.js
+++ Vanilla-weather-app/src/app.js
@@ -25,24 +25,30 @@ function formatDate(timeStamp){
 
 
 function displayTemperature(response){
-    console.log(response);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector ("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt*1000);
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 
 let apiKey = "f2a3c6239a67b0f60f690bbe099331c9";
+let city = "Lagos";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lagos,Nigeria&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
